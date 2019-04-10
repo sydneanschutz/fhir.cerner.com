6acf5deba20b4573f5668075cfc53e0868465426
Vinu George <vinu.m.george@gmail.com>
2019-04-10 14:41:26 -0400

    OPENSVC-8284 Update fhir.cerner.com

---
+++ content/millennium/r4/coverages/coverage.md
@@ -0,0 +1,75 @@
+---
+title: Coverage | R4 API
+---
+
+# Coverage
+
+* TOC
+{:toc}
+
+## Overview
+
+The Coverage resource provides ................
+
+The following fields are returned if valued:
+
+* [Coverage id](http://hl7.org/fhir/R4/coverage-definitions.html#Coverage.id){:target="_blank"}
+* [Status](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.status){:target="_blank"}
+* [Subscriber of policy](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.subscriber){:target="_blank"}
+* [Subscriber id](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.subscriberId){:target="_blank"}
+* [Plan beneficiary](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.beneficiary){:target="_blank"}
+* [Dependent number](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.dependent){:target="_blank"}
+* [Beneficiary relationship to subscriber](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.relationship){:target="_blank"}
+* [Start/end time of the coverage](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.period){:target="_blank"}
+* [Payor](https://hl7.org/fhir/r4/coverage-definitions.html#Coverage.payor){:target="_blank"}
+* [Class:](http://hl7.org/fhir/R4/coverage-definitions.html#Coverage.class){:target="_blank"}
+   * [Type](http://hl7.org/fhir/R4/coverage-definitions.html#Coverage.class.type){:target="_blank"}
+   * [Value](http://hl7.org/fhir/R4/coverage-definitions.html#Coverage.class.value){:target="_blank"}
+   * [Name](http://hl7.org/fhir/R4/coverage-definitions.html#Coverage.class.name){:target="_blank"}
+
+## Terminology Bindings
+
+<%= terminology_table(:coverage, :r4) %>
+   
+## Search
+
+Search for Coverages that meet supplied query parameters:
+
+    GET /Coverages?:parameters
+
+### Authorization Types
+
+<%= authorization_types(practitioner: true, patient: false, system: true)%>
+
+### Parameters
+
+ Name       | Required?                      | Type          | Description
+------------|--------------------------------|---------------|-------------------------------------------------------------------------------------------------------
+ `patient`  | Yes    | [`reference`] | The patient present at the coverage. Example: `12345`
+
+### Headers
+
+<%= headers fhir_json: true %>
+
+### Example
+
+#### Request
+
+    GET https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Coverage?patient=4342010
+
+#### Response
+
+<%= headers status: 200 %>
+<%= json(:r4_coverage_bundle) %>
+
+### Errors
+
+The common [errors] and [OperationOutcomes] may be returned.
+
+[`reference`]: https://hl7.org/fhir/r4/search.html#reference
+[errors]: ../../#client-errors
+[OperationOutcomes]: ../../#operation-outcomes
+
+### Body Fields
+
+<%= definition_table(:coverage, :create, :r4) %>
---
+++ lib/resources/example_json/r4_examples_coverage.rb
@@ -0,0 +1,79 @@
+module Cerner
+    module Resources
+  
+      R4_COVERAGE_BUNDLE ||= {
+        "resourceType": "Bundle",
+        "id": "ed51c7e2-bd88-4a00-8ee3-f2670190059f",
+        "type": "searchset",
+        "total": 1,
+        "link": [
+            {
+                "relation": "self",
+                "url": "http://localhost:3000/r4/2c400054-42d8-4e74-87b7-80b5bd5fde9f/Coverage?patient=1626016"
+            }
+        ],
+        "entry": [
+            {
+                "fullUrl": "http://localhost:3000/r4/2c400054-42d8-4e74-87b7-80b5bd5fde9f/Coverage/2764339",
+                "resource": {
+                    "resourceType": "Coverage",
+                    "id": "2764339",
+                    "status": "active",
+                    "meta": {
+                        "versionId": "16",
+                        "lastUpdated": "2015-05-04T22:44:32.000Z"
+                    },
+                    "text": {
+                        "status": "generated",
+                        "div": "&lt;div>&lt;p>&lt;b>Coverage&lt;/b>&lt;/p>&lt;p>&lt;b>Status&lt;/b>: active&lt;/p>&lt;p>&lt;b>Subscriber&lt;/b>: WOLFE, STEPHANIE T&lt;/p>&lt;p>&lt;b>Beneficiary&lt;/b>: Stephanie Wolfe&lt;/p>&lt;p>&lt;b>Dependent Number&lt;/b>: KEWIL4WW&lt;/p>&lt;p>&lt;b>Coverage Start Date&lt;/b>: Sep  5, 2012  9:30 A.M. CDT&lt;/p>&lt;p>&lt;b>Payor&lt;/b>: Aetna Missouri POS - Open&lt;/p>&lt;p>&lt;b>Class&lt;/b>: plan: Value: 729823 Name: Aetna Missouri POS Open&lt;/p>&lt;/div>"
+                    },
+                    "subscriber": {
+                        "reference": "Patient/1626016",
+                        "display": "WOLFE, STEPHANIE T"
+                    },
+                    "subscriberId": "1626016",
+                    "beneficiary": {
+                        "reference": "Patient/1626016",
+                        "display": "WOLFE, STEPHANIE T"
+                    },
+                    "dependent": "KEWIL4WW",
+                    "relationship": [
+                        {
+                            "coding": [
+                                {
+                                "system": "http://terminology.hl7.org/CodeSystem/subscriber-relationship",
+                                "code": "child"
+                                }
+                            ]
+                        }
+                    ],
+                    "period": {
+                        "start": "2012-09-05T14:30:12.000Z",
+                        "end": "2013-09-05T14:30:12.000Z"
+                    },
+                    "payor": {
+                        "reference": "Organization/643847",
+                        "display": "Aetna Missouri POS - Open"
+                    },
+                    "class": [
+                        {
+                            "type": {
+                                "coding": [
+                                    {
+                                        "system": "http://hl7.org/fhir/ValueSet/coverage-class",
+                                        "code": "plan"
+                                    }
+                                ],
+                                "text": "plan"
+                            },
+                            "value": "729823",
+                            "name": "Aetna Missouri POS Open"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    end
+  end
+  
